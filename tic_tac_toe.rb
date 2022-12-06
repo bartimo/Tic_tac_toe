317b52ee9df0aa7444b8e1f31438b3a89df9f09b
Bart Zweigoron <bzweigoron@gmail.com>
2022-12-05 21:36:50 -0500

    Create intro

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,20 +1,55 @@
+# Defines the Game object
+class Game
+  attr_accessor :round
+
+  def initialize
+    @round = 1
+  end
+end
+
+# Defines the Board object
 class Board
-  board_array = []
+  def initialize
+    @board = Array['1', '2', '3', '4', '5', '6', '7', '8', '9']
+  end
 
-
-
-
-  def displayBoard
-
-  end 
+  def display
+    puts '  A B C'
+    puts "1 #{@board[0]}|#{@board[1]}|#{@board[2]}"
+    puts "2 #{@board[3]}|#{@board[4]}|#{@board[5]}"
+    puts "3 #{@board[6]}|#{@board[7]}|#{@board[8]}"
+  end
 end
 
-class Square(col,row)
+# Defines Player Class
+class Player
+  attr_reader :name, :symbol
+
+  def initialize(name, symbol)
+    @name = name
+    @symbol = symbol
+  end
+end
+
+def display_intro
 
 end
 
+game = Game.new
+board = Board.new
 
-puts "  1 2 3"
-puts "A .|.|O"
-puts "B .|X|."
-puts "C .|.|."
+puts 'Welcome to Tic-Tac-Toe!'
+puts ''
+sleep(1)
+puts 'Player 1, enter your name:'
+player1 = Player.new(gets.chomp,'X')
+puts 'Player 2, enter your name:'
+player2 = Player.new(gets.chomp,'O')
+sleep(1)
+puts ''
+puts "#{player1.name} VS #{player2.name} BEGIN"
+sleep(1)
+puts ''
+puts "Turn #{game.round} - #{player1.name}"
+puts ''
+board.display
