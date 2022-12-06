e424a8f06806572acc7b5cb0a0ea78790ed48984
Bart Zweigoron <bzweigoron@gmail.com>
2022-12-06 17:23:48 -0500

    Cleaned up code

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -4,9 +4,7 @@ class Board
   attr_reader :round
 
   def initialize
-    #@board = Array['1', '2', '3', '4', '5', '6', '7', '8', '9']
-    #@board = Array.new(9, '.')
-    @board = Array.new(3, Array.new(3,'.'))
+    @board = [['.', '.', '.'], ['.', '.', '.'], ['.', '.', '.']]
     @round = 1
     @turn = 1
   end
@@ -21,11 +19,10 @@ class Board
   end
 
   def place_selection(board, selection, player)
-    @board[1][1] = "X"
-
+    @board[2][0] = 'X'
   end
 
-  def next_round()
+  def next_round
     @round += 1
   end
 end
@@ -40,28 +37,24 @@ class Player
   end
 end
 
-#Program Methods
-
+# Program Methods
 
 def get_selection(board,player)
   valid_selection = false
   puts "Round #{board.round} - #{player.name}"
   board.display
-  while not valid_selection
+  until valid_selection
     puts 'Enter your selection (Column/row)'
     selection = gets.chomp.upcase.split('')
-    if selection[0].between?('A','C') && selection[1].between?('1','3')  && selection.length == 2 then
+    if selection[0].between?('A', 'C') && selection[1].between?('1', '3') && selection.length == 2
       valid_selection = true
-   else
-     puts 'Invalid Selection - please enter using format "A1"'
-   end
+    else
+      puts 'Invalid Selection - please enter using format "A1"'
+    end
   end
   selection
 end
 
-
-
-#Convert
 def to_num_array(selection)
   selection[0] = selection[0].ord - 65
   selection[1] = selection[1].to_i - 1
@@ -72,24 +65,23 @@ board = Board.new
 
 puts 'Welcome to Tic-Tac-Toe!'
 puts ''
-#sleep(1)
-#puts 'Player 1, enter your name:'
-#player1 = Player.new(gets.chomp,'X')
-#puts 'Player 2, enter your name:'
-#player2 = Player.new(gets.chomp,'O')
-#sleep(1)
-#puts ''
-player1 = Player.new("Biffington" , 'X')
-player2 = Player.new("Houndsworth" , 'O')
-
+# sleep(1)
+# puts 'Player 1, enter your name:'
+# player1 = Player.new(gets.chomp,'X')
+# puts 'Player 2, enter your name:'
+# player2 = Player.new(gets.chomp,'O')
+# sleep(1)
+# puts ''
+player1 = Player.new('Biffington', 'X')
+player2 = Player.new('Houndsworth', 'O')
 
 puts "#{player1.name} VS #{player2.name} BEGIN"
 current_player = player1
-#sleep(1)
+# sleep(1)
 puts ''
 puts ''
 selection = get_selection(board, current_player)
-if selection then
+if selection
   selection = to_num_array(selection)
   placed = board.place_selection(board, selection, current_player)
   board.display
