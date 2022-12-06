e3a78bba494ae67256582e11b31b7a3d2a22ff17
Bart Zweigoron <bzweigoron@gmail.com>
2022-12-06 14:44:40 -0500

    started round functionality

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,23 +1,27 @@
-# Defines the Game object
-class Game
-  attr_accessor :round
-
-  def initialize
-    @round = 1
-  end
-end
-
+# rubocop:disable all
 # Defines the Board object
 class Board
+  attr_reader :round
+
   def initialize
-    @board = Array['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    #@board = Array['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    #@board = Array.new(9, '.')
+    @board = Array.new(3, Array.new(3,'.'))
+    @round = 1
+    @turn = 1
   end
 
   def display
+    puts ''
     puts '  A B C'
-    puts "1 #{@board[0]}|#{@board[1]}|#{@board[2]}"
-    puts "2 #{@board[3]}|#{@board[4]}|#{@board[5]}"
-    puts "3 #{@board[6]}|#{@board[7]}|#{@board[8]}"
+    puts "1 #{@board[0][0]}|#{@board[0][1]}|#{@board[0][2]}"
+    puts "2 #{@board[1][0]}|#{@board[1][1]}|#{@board[1][2]}"
+    puts "3 #{@board[2][0]}|#{@board[2][1]}|#{@board[2][2]}"
+    puts ''
+  end
+
+  def next_round()
+    @round += 1
   end
 end
 
@@ -31,25 +35,33 @@ class Player
   end
 end
 
-def display_intro
+#Program Methods
 
+def play_turn(board,player)
+  puts "Round #{board.round} - #{player.name}"
+  board.display
+  puts 'Enter your selection (Column/row)'
+  selection = gets.chomp
+  p selection
 end
 
-game = Game.new
 board = Board.new
 
 puts 'Welcome to Tic-Tac-Toe!'
 puts ''
-sleep(1)
-puts 'Player 1, enter your name:'
-player1 = Player.new(gets.chomp,'X')
-puts 'Player 2, enter your name:'
-player2 = Player.new(gets.chomp,'O')
-sleep(1)
-puts ''
+#sleep(1)
+#puts 'Player 1, enter your name:'
+#player1 = Player.new(gets.chomp,'X')
+#puts 'Player 2, enter your name:'
+#player2 = Player.new(gets.chomp,'O')
+#sleep(1)
+#puts ''
+player1 = Player.new("Biffington" , 'X')
+player2 = Player.new("Houndsworth" , ')')
 puts "#{player1.name} VS #{player2.name} BEGIN"
-sleep(1)
+#sleep(1)
 puts ''
-puts "Turn #{game.round} - #{player1.name}"
 puts ''
-board.display
+play_turn(board,player1)
+
+# rubocop:enable all
